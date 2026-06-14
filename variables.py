792bf7ad5b4f094e1350334152a9eb2049d29891
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-06-14 14:12:24 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,9 @@
+print("Variables in Python")
+sher = "Aryan"
+print(sher)
+bhigibilli = "ashika"
+print(bhigibilli)
+#variables cant start with a number
+#spaces are not allowed in variable names
+#special characters are not allowed in variable names
+#variable names are case-sensitive
