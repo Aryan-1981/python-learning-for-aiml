3e892802d8487a26c07236aacb01d92f98e0d763
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-06-26 21:38:44 +0530

    slicing concept

--- slicing.py
+++ slicing.py
@@ -1,2 +1,9 @@
 a="College"
 print(a[2:6:1])
+print(a[0:7:2])
+print(a[::2]) # it will print clee because of default values
+
+b= "Hello How are you"
+print(b[6:9:1]) # it will print How
+print(b[0:6:1]) # it will print Hello
+print(b[14:17:1]) # it will print you
