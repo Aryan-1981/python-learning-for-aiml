d55de8490aad09f35c10930da235f9de449a0ee4
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-06-28 19:49:05 +0530

    ousj

--- input_output_operator.py
+++ input_output_operator.py
@@ -10,3 +10,10 @@ print(f"My name is {name} and my age is {age}") # f-string formatting
 #input print
 a= input(" Enter your age:-") # input function
 print(f"Your age is {a} ") 
+
+#if you want to numbers you can change the datatypes manually as input gives data in string
+b= int(input(" give me a number :- "))
+print(b+1)
+# arthimetic operators
+#numbers :- float int complex
+# (+,_,/,*)
