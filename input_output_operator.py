ffddb3ae21d7be2ef0c1fd6a94fa72d04cd51902
Aryan-1981 <aryan1981bhargava@gmail.com>
2026-06-28 16:01:48 +0530

    input and output

---
+++ input_output_operator.py
@@ -0,0 +1,12 @@
+#output print
+
+name = "Aryan"
+age = 20
+print("hello world") # basic
+print("My name is ",name," and my age is ",age) # multiple values
+print(f"My name is {name} and my age is {age}") # f-string formatting
+
+
+#input print
+a= input(" Enter your age:-") # input function
+print(f"Your age is {a} ") 
